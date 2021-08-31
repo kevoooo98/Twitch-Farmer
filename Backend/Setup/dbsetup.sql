21f2c4361538db6647d9f7b1ade7075d4903a68c
kevoooo98 <kevinschulze837@gmail.com>
2021-08-31 09:31:46 +0200

    kram

---
+++ Backend/Setup/dbsetup.sql
@@ -0,0 +1,12 @@
+CREATE USER 'dbuser'@'%' IDENTIFIED BY 'geheim1';
+GRANT ALL PRIVILEGES ON *.* to 'dbuser'@'%' WITH GRANT OPTION;
+CREATE DATABASE twitchfarm;
+
+USE twitchfarm;
+
+CREATE TABLE streams (
+  ID_Stream int primary key auto_increment
+  url varchar(255),
+  watchtime int,
+  fav bit
+);
