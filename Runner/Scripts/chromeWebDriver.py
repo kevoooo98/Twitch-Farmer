b0f96bbdfaf109de9c5db8217acaf1dfadee0f81
kevoooo98 <kevinschulze837@gmail.com>
2021-09-06 20:59:16 +0200

    persist chrome profile

--- Runner/Scripts/chromeWebDriver.py
+++ Runner/Scripts/chromeWebDriver.py
@@ -13,6 +13,7 @@ class chromeWebDriver():
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument('--headless')
+        options.add_argument('user-data-dir=~/.config/google-chrome')
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
     def set_url (self, url):
